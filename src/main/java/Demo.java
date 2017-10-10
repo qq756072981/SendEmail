1082ae7831bb04502baa8cfe60c2b21350a7ad87
yangshu <yangshu985@gmail.com>
2017-10-10 17:34:40 +0800

    Initial commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+
+        sendEmail();
+    }
+
+    public static void sendEmail () {
+        System.out.println(222);
+    }
+}
